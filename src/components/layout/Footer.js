961f1dd5f3f1ad02e8a227cfb1f2170faea4c6e6
Sourabh Jyoti Das <dassourabh126@gmail.com>
2023-09-07 20:51:37 +0530

    Fix Responsive issues

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -5,28 +5,26 @@ import { GrAppleAppStore } from "react-icons/gr"
 export default function Footer() {
       return (
             <>
-                  <footer className='flex flex-col md:flex-row md:justify-around space-y-8 bg-purple-900 text-white py-5 px-5'>
-                        <div>
-                              <div className='left'>
-                                    <h4 className='capitalize primaryText'>Download our app</h4>
-                                    <p>Download app for  Android and Mobile Phones</p>
-                                    <button className='py-2 px-3 rounded-md bg-blue-400 text-white'> <AiFillAndroid className='text-2xl' /></button>
-                                    <button className='py-2 px-3 rounded-md mx-5 bg-blue-400 text-white'><GrAppleAppStore className='text-2xl' /></button>
+                  <footer className='flex flex-col md:flex-row md:items-center md:justify-around  space-y-5 md:space-y-0 bg-purple-900 text-white py-5 px-5 text-xs '>
+                        <div className='space-y-2 '>
+                              <h4 className='capitalize primaryText'>Download our app</h4>
+                              <p>Download app for  Android and Mobile Phones</p>
+                              <button className='py-2 px-3 rounded-md bg-blue-400 text-white'> <AiFillAndroid className='text-xs' /></button>
+                              <button className='py-2 px-3 rounded-md mx-5 bg-blue-400 text-white'><GrAppleAppStore className='text-xs' /></button>
 
-                              </div>
-                              <div className='mid'>
-                                    <div className='cursor-pointer  primaryText'>SmartBuyer</div>
-                                    <p>High quality is our first priority</p>
-                                    <p>Copyright 2027 &copy;  SmartBuyer</p>
-                                    <p>Design by <span>Sourabh Jyoti Das</span></p>
-                              </div>
                         </div>
-                        <div className='right list-none'>
-                              <div className='cursor-pointer  primaryText'>Lets Connect</div>
-                              <li>Instagram</li>
-                              <li>Youtube</li>
-                              <li>Twitter</li>
-                              <li>LinkedIn</li>
+                        <div className=' space-y-1 md:space-y-2'>
+                              <div className='cursor-pointer  primaryText'>SmartBuyer</div>
+                              <p>High quality is our first priority</p>
+                              <p>Copyright 2027 &copy;  SmartBuyer</p>
+                              <p>Design by <span>Sourabh Jyoti Das</span></p>
+                        </div>
+                        <div className='list-none space-y-1 md:space-y-2'>
+                              <div className='primaryText'>Lets Connect</div>
+                              <li className='cursor-pointer  primaryText'>Instagram</li>
+                              <li className='cursor-pointer  primaryText'>Youtube</li>
+                              <li className='cursor-pointer  primaryText'>Twitter</li>
+                              <li className='cursor-pointer  primaryText'>LinkedIn</li>
                         </div>
                   </footer>
             </>
